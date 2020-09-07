de8df2ebe6aec0b3f8403aa8da5b836c4ecb0049
Eugenio <eebf1993@gmail.com>
2020-09-07 19:07:49 -0400

    Modify version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import setuptools
 
 
-VERSION = os.environ.get("VERSION", "0.2.0")
+VERSION = os.environ.get("VERSION", "0.2.1")
 CDK_VERSION = os.environ.get("CDK_VERSION", "1.62.0")
 
 with open("./README.md") as fp:
